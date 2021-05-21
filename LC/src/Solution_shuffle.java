61b29ea8e42317e61a9c9afc7bad282e9f917c8a
paulr <paulr@DESKTOP-801UKC1>
2021-05-20 22:00:14 -0700

    Shuffled...DONE!

---
+++ LC/src/Solution_shuffle.java
@@ -0,0 +1,17 @@
+
+public class Solution_shuffle {
+	public int[] shuffle(int[] nums, int n) {
+        int[] shuffled = new int[nums.length];
+        int i = 0;
+        int firstIndex = 0;
+        int secondIndex = n;
+        while(i < n) {
+            shuffled[firstIndex] = nums[i];
+            shuffled[firstIndex + 1] = nums[secondIndex];
+            firstIndex += 2;
+            secondIndex++;
+            i++;
+        }
+        return shuffled;
+    }
+}
