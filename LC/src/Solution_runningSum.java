7f6571f4a7360afc95d5e281a50559dbca9a4438
paulr <paulr@DESKTOP-801UKC1>
2021-05-20 21:20:21 -0700

    SOLVED DUDE

---
+++ LC/src/Solution_runningSum.java
@@ -0,0 +1,12 @@
+
+public class Solution_runningSum {
+	public int[] runningSum(int[] nums) {
+        int[] sumArr = new int[nums.length];
+        int runningSum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            runningSum += nums[i];
+            sumArr[i] = runningSum;
+        }
+        return sumArr;
+    }
+}
