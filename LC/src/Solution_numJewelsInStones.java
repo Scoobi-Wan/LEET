1ac794838e4bbd22c4d73242cc47e4fa5d07195f
paulr <paulr@DESKTOP-801UKC1>
2021-05-20 22:14:22 -0700

    Solved another one

---
+++ LC/src/Solution_numJewelsInStones.java
@@ -0,0 +1,11 @@
+
+public class Solution_numJewelsInStones {
+	public int numJewelsInStones(String jewels, String stones) {
+        int numOfStoneJewels = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            if (jewels.contains(String.valueOf(stones.charAt(i)))) {
+                numOfStoneJewels++;
+            }
+         } return numOfStoneJewels;
+    }
+}
